beed8d96691e8171de2b1ff4b21ec010418c2d98
mayoi7 <3451669860@qq.com>
2019-02-01 12:08:15 +0800

    init(#user): add base_controller

---
+++ src/main/java/com/akira/kioku/controller/BaseController.java
@@ -0,0 +1,20 @@
+package com.akira.kioku.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 控制主页及其他一部分页面的跳转
+ * @author Kripath
+ * @date Created in 12:02 2019/2/1
+ */
+@Controller
+@Slf4j
+public class BaseController {
+
+    @RequestMapping("")
+    public String index() {
+        return "index";
+    }
+}
