b78e94333f33f11043040e7e3abfc859f97be498
mayoi7 <3451669860@qq.com>
2019-02-01 20:57:18 +0800

    init(#user): init user_service

---
+++ src/main/java/com/akira/kioku/service/UserService.java
@@ -0,0 +1,8 @@
+package com.akira.kioku.service;
+
+/**
+ * @author Kripath
+ * @date Created in 16:58 2019/2/1
+ */
+public interface UserService {
+}
---
+++ src/main/java/com/akira/kioku/service/impl/UserServiceImpl.java
@@ -0,0 +1,15 @@
+package com.akira.kioku.service.impl;
+
+import com.akira.kioku.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Kripath
+ * @date Created in 16:58 2019/2/1
+ */
+@Service
+@Slf4j
+public class UserServiceImpl implements UserService {
+
+}
