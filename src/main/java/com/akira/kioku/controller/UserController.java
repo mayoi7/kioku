add68504c5a79f152c0a2d37f40517ccf0deb9a4
mayoi7 <3451669860@qq.com>
2019-02-01 11:51:05 +0800

    init(#user): add user_controller

---
+++ src/main/java/com/akira/kioku/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.akira.kioku.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Kripath
+ * @date Created in 11:43 2019/2/1
+ */
+@RestController
+@RequestMapping("user")
+@Slf4j
+public class UserController {
+
+}
