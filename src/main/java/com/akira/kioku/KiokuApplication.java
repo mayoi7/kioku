1c504a00ab978d521509e5cf221fa26cee2342af
mayoi7 <3451669860@qq.com>
2019-02-01 11:25:28 +0800

    other: update gitignore and other

--- src/main/java/com/akira/kioku/KiokuApplication.java
+++ src/main/java/com/akira/kioku/KiokuApplication.java
@@ -3,6 +3,9 @@ package com.akira.kioku;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author 刘昊楠
+ */
 @SpringBootApplication
 public class KiokuApplication {
 
