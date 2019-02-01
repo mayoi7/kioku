10b033666a204a64823ca223f847d6cd8dab2684
mayoi7 <3451669860@qq.com>
2019-02-01 09:14:12 +0800

    Initial commit

---
+++ src/main/java/com/akira/kioku/KiokuApplication.java
@@ -0,0 +1,14 @@
+package com.akira.kioku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiokuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KiokuApplication.class, args);
+    }
+
+}
+
