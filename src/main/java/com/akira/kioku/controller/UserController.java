92fdc703e74223f89ad6dfa0751ee1b60125a1da
mayoi7 <3451669860@qq.com>
2019-02-02 12:36:28 +0800

    test(#shiro): add test methods to return msg

--- src/main/java/com/akira/kioku/controller/UserController.java
+++ src/main/java/com/akira/kioku/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.akira.kioku.controller;
 
+import com.akira.kioku.utils.ResultUtil;
+import com.akira.kioku.vo.ResultVo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,4 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class UserController {
 
+    /**
+     * 测试获取用户信息
+     * @return 返回成功的json
+     */
+    @GetMapping("msg")
+    public ResultVo getMsg() {
+        return ResultUtil.success();
+    }
 }
