570c557d177a058e8f0b1da531e8cfdc459a550e
mayoi7 <3451669860@qq.com>
2019-02-02 11:27:57 +0800

    init(#shiro): init shiro config

---
+++ src/main/java/com/akira/kioku/config/ShiroConfig.java
@@ -0,0 +1,110 @@
+package com.akira.kioku.config;
+
+import com.akira.kioku.shiro.CustomRealm;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+
+/**
+ * @author 刘昊楠
+ */
+@Configuration
+@Slf4j
+public class ShiroConfig {
+
+    @Bean
+    public ShiroFilterFactoryBean shirFilter(SecurityManager securityManager) {
+        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
+        // 必须设置 SecurityManager
+        shiroFilterFactoryBean.setSecurityManager(securityManager);
+        // setLoginUrl 如果不设置值，默认会自动寻找Web工程根目录下的"/login.jsp"页面 或 "/login" 映射
+        shiroFilterFactoryBean.setLoginUrl("/login");
+        // 设置无权限时跳转的 url;
+        shiroFilterFactoryBean.setUnauthorizedUrl("");
+
+        // 设置拦截器
+        Map<String, String> filterChainDefinitionMap = new LinkedHashMap<>();
+        // 用户，需要角色权限 “user”且已登陆
+        filterChainDefinitionMap.put("/user/**", "authc, roles[user]");
+        // 管理员，需要角色权限 “admin”且已登录
+        filterChainDefinitionMap.put("/admin/**", "authc, roles[admin]");
+        // 开放登陆接口
+        filterChainDefinitionMap.put("/login/**", "anon");
+        // 开放测试接口
+        // isRemember() || isAuthentication()
+         filterChainDefinitionMap.put("/test/remember", "user, roles[user]");
+        // 其余接口一律拦截
+        // 主要这行代码必须放在所有权限设置的最后，不然会导致所有 url 都被拦截
+        filterChainDefinitionMap.put("/**", "authc");
+
+        shiroFilterFactoryBean.setFilterChainDefinitionMap(filterChainDefinitionMap);
+        log.info("[配置]Shiro拦截器工厂类注入");
+        return shiroFilterFactoryBean;
+    }
+
+    /**
+     * 注入 securityManager
+     */
+    @Bean
+    public SecurityManager securityManager() {
+        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
+        // 设置realm.
+        securityManager.setRealm(customRealm());
+        return securityManager;
+    }
+
+    /**
+     * 凭证匹配器
+     * 密码校验交给Shiro的SimpleAuthenticationInfo进行处理
+     *
+     */
+    @Bean
+    public HashedCredentialsMatcher hashedCredentialsMatcher() {
+        HashedCredentialsMatcher hashedCredentialsMatcher = new HashedCredentialsMatcher();
+        // 散列算法:这里使用MD5算法;
+        hashedCredentialsMatcher.setHashAlgorithmName("md5");
+        // 散列的次数，比如散列两次，相当于 md5(md5(""));
+        hashedCredentialsMatcher.setHashIterations(1);
+        return hashedCredentialsMatcher;
+    }
+
+    /**
+     * 自定义身份认证 realm;
+     * 必须写这个类，并加上 @Bean 注解，目的是注入 CustomRealm，
+     * 否则会影响 CustomRealm类 中其他类的依赖注入
+     */
+    @Bean
+    public CustomRealm customRealm() {
+        CustomRealm customRealm = new CustomRealm();
+        // 使用md5加密
+        customRealm.setCredentialsMatcher(hashedCredentialsMatcher());
+        return customRealm;
+    }
+
+    /**
+     * 下面来两个是为了使@RequiresRoles注解生效
+     */
+    @Bean
+    public DefaultAdvisorAutoProxyCreator advisorAutoProxyCreator(){
+        DefaultAdvisorAutoProxyCreator advisorAutoProxyCreator = new DefaultAdvisorAutoProxyCreator();
+        advisorAutoProxyCreator.setProxyTargetClass(true);
+        return advisorAutoProxyCreator;
+    }
+
+    @Bean
+    public AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor(SecurityManager securityManager) {
+        AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor = new AuthorizationAttributeSourceAdvisor();
+        authorizationAttributeSourceAdvisor.setSecurityManager(securityManager);
+        return authorizationAttributeSourceAdvisor;
+    }
+}
---
+++ src/main/java/com/akira/kioku/shiro/CustomRealm.java
@@ -0,0 +1,94 @@
+package com.akira.kioku.shiro;
+
+
+import com.akira.kioku.enums.RoleEnum;
+import com.akira.kioku.po.User;
+import com.akira.kioku.service.UserService;
+import com.akira.kioku.utils.EnumUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.*;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.util.ByteSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.core.EmitUtils;
+
+import javax.management.relation.Role;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ * 自定义shiro的验证规则
+ * @author 刘昊楠
+ */
+@Slf4j
+public class CustomRealm extends AuthorizingRealm {
+    private UserService userService;
+
+    @Autowired
+    private void setUserMapper(UserService userService) {
+        this.userService = userService;
+    }
+
+    /**
+     * 获取身份验证信息
+     * Shiro中，最终是通过 Realm 来获取应用程序中的用户、角色及权限信息的。
+     *
+     * @param authenticationToken 用户身份信息 token
+     * @return 返回封装了用户信息的 AuthenticationInfo 实例
+     */
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        System.out.println("————身份认证方法————");
+        UsernamePasswordToken token = (UsernamePasswordToken) authenticationToken;
+        // 从数据库获取对应用户名密码的用户
+        User user = userService.findByUsername(token.getUsername());
+        if(user == null) {throw new UnknownAccountException();}
+
+        String password = user.getPassword();
+//        if (!password.equals(new String((char[]) token.getCredentials()))) {
+//            throw new IncorrectCredentialsException();
+//        }
+        //盐值
+        ByteSource credentialsSalt = ByteSource.Util.bytes(user.getUsername());
+
+        return new SimpleAuthenticationInfo(token.getPrincipal(), password, credentialsSalt,
+                                            getName());
+    }
+
+    /**
+     * 获取授权信息
+     */
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        System.out.println("————权限认证————");
+        String username = (String) SecurityUtils.getSubject().getPrincipal();
+        //获得该用户角色
+        Integer role = userService.findRoleByUsername(username);
+        Set<String> roles = new HashSet<>();
+
+        //需要将 role 封装到 Set 作为 info.setRoles() 的参数
+        RoleEnum roleEnum = EnumUtil.getByCode(role, RoleEnum.class);
+
+        // 该用户的权限码错误
+        if(roleEnum == null) {
+            log.error("[权限]用户{}在数据库中的权限数据异常，权限码为{}", username, role);
+        } else {
+            roles.add(roleEnum.getMsg());
+            // 普通用户的权限码
+            Integer normalUserCode = 11;
+
+            // 赋予该用户所有低级权限
+            for (int i = normalUserCode; i < role; i++) {
+                roles.add(Objects.requireNonNull(EnumUtil.getByCode(i, RoleEnum.class)).getMsg());
+            }
+        }
+
+        //设置该用户拥有的角色
+        return new SimpleAuthorizationInfo(roles);
+    }
+}
