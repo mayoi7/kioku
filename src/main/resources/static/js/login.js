9f00f398be111294835d6acc3af3f40345cd8862
mayoi7 <3451669860@qq.com>
2019-02-02 20:08:49 +0800

    save(#login): modified some part in login page

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,8 +1,16 @@
 /*jshint esversion: 6 */
 var app = new Vue({
-  el: '#app',
-  data: {
+    el: '#app',
+    data: {
     'username': '',
     'password': ''
-  }
+    },
+    methods: {
+      submit: function (e) {
+        e.preventDefault();
+        fetch('/test/msg').then(response => response.json()).then(data => {
+            console.log(data);
+        });
+      }
+    }
 });
