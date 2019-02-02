ac197e197fbf14fd9eed242b2d822249075a21bd
mayoi7 <3451669860@qq.com>
2019-02-02 11:11:23 +0800

    opt(#role): modified role enum and message

--- src/main/java/com/akira/kioku/enums/RoleEnum.java
+++ src/main/java/com/akira/kioku/enums/RoleEnum.java
@@ -13,24 +13,25 @@ import lombok.Getter;
 public enum RoleEnum implements CodeEnum {
 
     /**
+     * 被锁定的用户，不能够进行日记的添加操作，仅拥有查看自己日记的权限
+     */
+    LOCKED_USER(10, "locked"),
+
+    /**
      * 普通用户
      */
-    NORMAL_USER(10, "普通用户"),
+    NORMAL_USER(11, "user"),
 
     /**
      * 普通管理员，拥有查看和锁定用户的权利
      */
-    NORMAL_ADMIN(11, "普通管理员"),
+    NORMAL_ADMIN(12, "admin"),
 
-    /**
-     * 被锁定的用户，不能够进行日记的添加操作，仅拥有查看自己日记的权限
-     */
-    LOCKED_USER(12, "被锁定的用户"),
 
     /**
      * 超级管理员，拥有包括赋予其他人管理员在内的所有权限
      */
-    SUPER_ADMIN(13, "超级管理员"),
+    SUPER_ADMIN(13, "sp_admin"),
     ;
 
     private final Integer code;
