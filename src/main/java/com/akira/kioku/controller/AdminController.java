7eda033675e59a0f35a6dde0679e8ca00ae43377
mayoi7 <3451669860@qq.com>
2019-02-02 12:10:12 +0800

    init(#admin): init admin_controller

---
+++ src/main/java/com/akira/kioku/controller/AdminController.java
@@ -0,0 +1,14 @@
+package com.akira.kioku.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 管理员的一系列操作
+ * @author Kripath
+ * @date Created in 12:09 2019/2/2
+ */
+@RestController
+@RequestMapping("admin")
+public class AdminController {
+}
