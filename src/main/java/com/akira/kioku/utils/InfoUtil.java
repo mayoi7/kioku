247a5d4e7fe3d0c17130739d06702a13ad2dea96
mayoi7 <3451669860@qq.com>
2019-02-03 12:02:42 +0800

    feat(#user): add a method of encapsulating user information

---
+++ src/main/java/com/akira/kioku/utils/InfoUtil.java
@@ -0,0 +1,29 @@
+package com.akira.kioku.utils;
+
+import com.akira.kioku.po.User;
+
+/**
+ * 负责对pojo进行一些处理
+ * @author Kripath
+ * @date Created in 11:56 2019/2/3
+ */
+public class InfoUtil {
+
+    /**
+     * 将用户信息封装成用户对象，同时对密码进行加密
+     * @return 封装后的用户对象
+     */
+    public static User encryptAndStorageAsUser(String username, String password,
+                                               String email, String code) {
+        // 对密码进行加密，用户名做盐值
+        password = TokenUtil.makeTEncryptTokenBySaltedMd5(password, username).toString();
+
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setEmail(email);
+        user.setCode(code);
+
+        return user;
+    }
+}
