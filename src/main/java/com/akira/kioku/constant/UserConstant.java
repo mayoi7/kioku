acc20a7d0b827faafb5874a25501bdad5354505f
mayoi7 <3451669860@qq.com>
2019-02-03 12:56:55 +0800

    refactor(#role): use constant class to save role enum

---
+++ src/main/java/com/akira/kioku/constant/UserConstant.java
@@ -0,0 +1,17 @@
+package com.akira.kioku.constant;
+
+/**
+ * 关于用户的常量
+ * @author Kripath
+ * @date Created in 12:52 2019/2/3
+ */
+public class UserConstant {
+
+    public static final int LOCKED_ROLE = 10;
+
+    public static final int USER_ROLE = 11;
+
+    public static final int ADMIN_ROLE = 12;
+
+    public static final int SUPER_ADMIN_ROLE = 13;
+}
--- src/main/java/com/akira/kioku/enums/RoleEnum.java
+++ src/main/java/com/akira/kioku/enums/RoleEnum.java
@@ -1,5 +1,6 @@
 package com.akira.kioku.enums;
 
+import com.akira.kioku.constant.UserConstant;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -15,23 +16,23 @@ public enum RoleEnum implements CodeEnum {
     /**
      * 被锁定的用户，不能够进行日记的添加操作，仅拥有查看自己日记的权限
      */
-    LOCKED_USER(10, "locked"),
+    LOCKED_USER(UserConstant.LOCKED_ROLE, "locked"),
 
     /**
      * 普通用户
      */
-    NORMAL_USER(11, "user"),
+    NORMAL_USER(UserConstant.USER_ROLE, "user"),
 
     /**
      * 普通管理员，拥有查看和锁定用户的权利
      */
-    NORMAL_ADMIN(12, "admin"),
+    NORMAL_ADMIN(UserConstant.ADMIN_ROLE, "admin"),
 
 
     /**
      * 超级管理员，拥有包括赋予其他人管理员在内的所有权限
      */
-    SUPER_ADMIN(13, "sp_admin"),
+    SUPER_ADMIN(UserConstant.SUPER_ADMIN_ROLE, "sp_admin"),
     ;
 
     private final Integer code;
