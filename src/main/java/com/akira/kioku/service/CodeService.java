da78d900962d2e3af81eb3855796e2bcfa7fb51c
mayoi7 <3451669860@qq.com>
2019-02-03 11:07:35 +0800

    init(#code): init code_service

---
+++ src/main/java/com/akira/kioku/service/CodeService.java
@@ -0,0 +1,9 @@
+package com.akira.kioku.service;
+
+/**
+ * 关于邀请码的检验相关的方法
+ * @author Kripath
+ * @date Created in 11:06 2019/2/3
+ */
+public interface CodeService {
+}
---
+++ src/main/java/com/akira/kioku/service/impl/CodeServiceImpl.java
@@ -0,0 +1,10 @@
+package com.akira.kioku.service.impl;
+
+import com.akira.kioku.service.CodeService;
+
+/**
+ * @author Kripath
+ * @date Created in 11:07 2019/2/3
+ */
+public class CodeServiceImpl implements CodeService {
+}
