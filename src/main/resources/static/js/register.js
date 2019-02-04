663b0930d804588a00dab70fdcab54e1d521cd28
mayoi7 <3451669860@qq.com>
2019-02-04 16:53:19 +0800

    opt(#register): add prompts next to the input

--- src/main/resources/static/js/register.js
+++ src/main/resources/static/js/register.js
@@ -6,7 +6,14 @@ const re_password = /^(?:\d+|[a-zA-Z]+|[!@#$%^&*]+){6,20}$/;
 var app = new Vue({
     el: '#app',
     data: {
-      'errors': [false, false, false, false, false],
+      'errors':
+          [
+              {isErr: false, msg: ''},
+              {isErr: false, msg: ''},
+              {isErr: false, msg: ''},
+              {isErr: false, msg: ''},
+              {isErr: false, msg: ''}
+          ],
       'user': {
           username: '',
           password: '',
@@ -18,7 +25,11 @@ var app = new Vue({
     methods: {
       submit: function(e) {
         this.validAll();
-        if(this.errors[0] || this.errors[1] || this.errors[2] || this.errors[3] || this.errors[4]) {
+        if (this.errors[0].isErr ||
+            this.errors[1].isErr ||
+            this.errors[2].isErr ||
+            this.errors[3].isErr ||
+            this.errors[4].isErr) {
           return false;
         } else {
           $.post("login/register", this.user, (data) => {
@@ -35,32 +46,45 @@ var app = new Vue({
       validUsername: function() {
         /*不匹配*/
         // if(!re_username.test(this.username)) this.errors[0] = true;
-        this.errors[0] = (!re_username.test(this.user.username));
+        this.errors[0].isErr = (!re_username.test(this.user.username));
+        this.errors[0].msg = '用户名不合法';
 
         /*异步校验用户名是否重复*/
           $.get("/login/detect/name/" + this.user.username, (data) => {
             // 假如用户名重复（code为1），将错误标志置为真
-            this.errors[0] = (data.code !== 0);
+            if(data.code === 0) {
+                this.errors[0].msg = '可用';
+                this.errors[0].isErr = false;
+            } else {
+                this.errors[0].msg = '用户名被使用';
+                this.errors[0].isErr = true;
+            }
           });
       },
       validPassword: function() {
-        this.errors[1] = (!re_password.test(this.user.password));
+        this.errors[1].isErr = (!re_password.test(this.user.password));
       },
       validRepeat: function() {
-        this.errors[2] = (this.user.repeat !== this.user.password);
+        this.errors[2].isErr = (this.user.repeat !== this.user.password);
       },
       validEmail: function () {
-        this.errors[3] = (!re_email.test(this.user.email));
+        this.errors[3].isErr = (!re_email.test(this.user.email));
       },
       validCode: function() {
         /* 异步校验邀请码合法性 */
           $.get("/login/detect/code/" + this.user.code, (data) => {
               // 假如用户名重复（code为1），将错误标志置为真
-              this.errors[4] = (data.code !== 0);
+              this.errors[4].isErr = (data.code !== 0);
           });
       },
       validAll: function() {
-        this.errors = [false, false, false, false, false];
+        this.errors = [
+            {isErr: false, msg: ''},
+            {isErr: false, msg: ''},
+            {isErr: false, msg: ''},
+            {isErr: false, msg: ''},
+            {isErr: false, msg: ''}
+        ];
         this.validUsername();
         this.validPassword();
         this.validRepeat();
