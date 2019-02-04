ddb1d74728ee377e45126f62ef7eb474cb0d85d7
mayoi7 <3451669860@qq.com>
2019-02-04 12:10:55 +0800

    other(#login): solve some warns

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,12 +1,12 @@
 /*jshint esversion: 6 */
-var app = new Vue({
+let app = new Vue({
     el: '#app',
     data: {
     'username': '',
     'password': ''
     },
     methods: {
-      submit: function (e) {
+      submit: function () {
         if(this.username.length === 0 || this.password.length === 0) {
             alert("用户名和密码不能为空");
             return false;
