c4bc36e63e0276606c798d29ea3c4ff46644fc65
mayoi7 <3451669860@qq.com>
2019-02-06 10:09:13 +0800

    feat(#note): init note controller

---
+++ src/main/java/com/akira/kioku/controller/NoteController.java
@@ -0,0 +1,15 @@
+package com.akira.kioku.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 关于note的操作
+ * @author Kripath
+ * @date Created in 10:07 2019/2/6
+ */
+@RestController
+@RequestMapping("note")
+public class NoteController {
+
+}
