2cacb74885d71d16cd3c92d18bfa612987ed2b9a
mayoi7 <3451669860@qq.com>
2019-02-06 15:17:53 +0800

    feat(#note): add method to return notes in page

--- src/main/java/com/akira/kioku/repository/NoteRepository.java
+++ src/main/java/com/akira/kioku/repository/NoteRepository.java
@@ -3,10 +3,14 @@ package com.akira.kioku.repository;
 import com.akira.kioku.dto.NoteInfo;
 import com.akira.kioku.dto.NotePackageable;
 import com.akira.kioku.po.Note;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * @author Kripath
@@ -15,8 +19,13 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface NoteRepository extends JpaRepository<Note, Long> {
 
-    @Query(value = "SELECT a.id, a.title, b.content, a.gmt_create gmtCreate " +
-            "FROM note a, content b " +
-            "WHERE a.id = b.nid AND a.id = :id", nativeQuery = true)
-    NotePackageable findByIdAsInfo(@Param("id") Long id);
+    /**
+     * 查询某用户的所有note
+     * @param uid 作者id
+     * @param pageable 分页类
+     * @return {@link NotePackageable}的集合
+     */
+    @Query(value = "SELECT note.id, note.title, content, note.gmt_create gmtCreate " +
+            "FROM note, content WHERE note.id = content.nid AND note.uid = 2", nativeQuery = true)
+    Page<NotePackageable> findByUidAsInfo(@Param("uid") Long uid, Pageable pageable);
 }
