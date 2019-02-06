3584a13d88753077eb6693f01eb02282d6c0a67e
mayoi7 <3451669860@qq.com>
2019-02-06 10:02:19 +0800

    feat(#user): add method to get username of logged in user

--- src/main/java/com/akira/kioku/controller/UserController.java
+++ src/main/java/com/akira/kioku/controller/UserController.java
@@ -3,6 +3,8 @@ package com.akira.kioku.controller;
 import com.akira.kioku.utils.ResultUtil;
 import com.akira.kioku.vo.ResultVo;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,5 +25,19 @@ public class UserController {
     @GetMapping("msg")
     public ResultVo getMsg() {
         return ResultUtil.success();
+    }
+
+    /**
+     * 获取登陆的用户名
+     * @return 已登录登陆用户的用户名，如果未登陆就返回错误码
+     */
+    @GetMapping("name")
+    public ResultVo returnUserName() {
+        Subject subject = SecurityUtils.getSubject();
+        if(!subject.isAuthenticated()) {
+            // 用户未登陆
+            return ResultUtil.error("未登陆的用户");
+        }
+        return ResultUtil.success(subject.getPrincipal());
     }
 }
