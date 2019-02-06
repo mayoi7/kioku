f65968c357336ebc0ed2828d8005041b89a6c9a5
mayoi7 <3451669860@qq.com>
2019-02-06 12:27:46 +0800

    fix(#user): add missing author id(uid)

--- src/main/java/com/akira/kioku/po/Note.java
+++ src/main/java/com/akira/kioku/po/Note.java
@@ -22,6 +22,8 @@ public class Note implements Serializable {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 
+	private Long uid;
+
 	private String title;
 
 	@JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
