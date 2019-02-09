64191be7b216d20aa29b2b03deb8eb71208fb8a4
mayoi7 <3451669860@qq.com>
2019-02-09 10:45:04 +0800

    feat(#note): make form submit

--- src/main/resources/static/js/add.js
+++ src/main/resources/static/js/add.js
@@ -1,18 +1,34 @@
 /*jshint esversion: 6 */
 var $app = new Vue({
   el: '#app',
-  data: {
-    title: 'sd fds收到',
-    content: '456789456'
+  data: function () {
+      $.get("/user/info", (data) => {
+          if(data.code === 0) {
+              this.user = data.data;
+          }
+      });
+      return {
+          user: {},
+          note: {
+              title: '',
+              content:  ''
+          }
+      }
   },
   methods: {
     submit: function(event) {
-      if(!this.title.length || !this.content.length) {
-        alert("输入内容不能为空");
+      if(!this.note.title.length || !this.note.content.length) {
+          alert("输入内容不能为空");
       } else {
+        $.post("/note/" + this.user.id, this.note, (data) => {
+            if(data.code === 0) {
+                window.location = "/";
+            } else {
+                alert(data.msg);
+            }
+        });
         return true;
       }
-      event.preventDefault();
     }
   }
 });
