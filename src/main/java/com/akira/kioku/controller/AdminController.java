6e813bd7748a622a4c9497481f4a84e5ac019ed3
mayoi7 <3451669860@qq.com>
2019-02-13 21:35:46 +0800

    feat(#admin): add methods to count base data
    include online user, registered user and note counts

--- src/main/java/com/akira/kioku/controller/AdminController.java
+++ src/main/java/com/akira/kioku/controller/AdminController.java
@@ -1,8 +1,12 @@
 package com.akira.kioku.controller;
 
+import com.akira.kioku.service.NoteService;
+import com.akira.kioku.service.UserService;
+import com.akira.kioku.shiro.ShiroSessionListener;
 import com.akira.kioku.utils.ResultUtil;
 import com.akira.kioku.vo.ResultVo;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -25,6 +29,20 @@ import java.net.URLEncoder;
 @Slf4j
 public class AdminController {
 
+    private final ShiroSessionListener shiroSessionListener;
+
+    private final UserService userService;
+
+    private final NoteService noteService;
+
+    @Autowired
+    public AdminController(ShiroSessionListener shiroSessionListener,
+                           UserService userService, NoteService noteService) {
+        this.shiroSessionListener = shiroSessionListener;
+        this.userService = userService;
+        this.noteService = noteService;
+    }
+
     /**
      * 测试获取管理员信息
      * @return 返回成功的json
@@ -34,10 +52,37 @@ public class AdminController {
         return ResultUtil.success();
     }
 
+    /**
+     * 跳转到管理员页面
+     */
     @RequestMapping("")
     public ModelAndView goAdminPage() {
         return new ModelAndView("admin/admin");
     }
 
+    /**
+     * 统计在线人数，通过session来统计
+     */
+    @GetMapping("/count/online")
+    public Long countOnline() {
+        return (long) shiroSessionListener.getSessionCount().get();
+    }
+
+    /**
+     * 统计注册人数
+     */
+    @GetMapping("/count/register")
+    public Long countRegister() {
+        return userService.countRegisterUser();
+    }
+
+    /**
+     * 统计日记总数
+     * @return 数据库中note表的记录总数
+     */
+    @GetMapping("/count/note")
+    public Long countNote() {
+        return noteService.countNote();
+    }
 
 }
