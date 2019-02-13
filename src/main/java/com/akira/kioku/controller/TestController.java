2fc136f8bbc6f8e1a9f5325f5ea90a4111fa0e80
mayoi7 <3451669860@qq.com>
2019-02-13 20:08:30 +0800

    fix(#shiro): fix bug of session cannot be found

--- src/main/java/com/akira/kioku/controller/TestController.java
+++ src/main/java/com/akira/kioku/controller/TestController.java
@@ -1,8 +1,10 @@
 package com.akira.kioku.controller;
 
+import com.akira.kioku.shiro.ShiroSessionListener;
 import com.akira.kioku.utils.ResultUtil;
 import com.akira.kioku.vo.ResultVo;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -23,13 +25,20 @@ import java.net.URLEncoder;
 @Slf4j
 public class TestController {
 
+    private final ShiroSessionListener shiroSessionListener;
+
+    @Autowired
+    public TestController(ShiroSessionListener shiroSessionListener) {
+        this.shiroSessionListener = shiroSessionListener;
+    }
+
     @GetMapping("msg")
     public ResultVo getMessage() {
         return ResultUtil.success();
     }
 
     @GetMapping("/count")
-    public String number(HttpServletResponse resp, HttpSession session){
-        return "";
+    public Integer number(HttpServletResponse resp, HttpSession session){
+        return shiroSessionListener.getSessionCount().get();
     }
 }
