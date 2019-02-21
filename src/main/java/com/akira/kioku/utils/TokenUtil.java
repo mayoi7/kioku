695b710c86d394d9c6b6b1c19614796f0b53b224
mayoi7 <3451669860@qq.com>
2019-02-21 22:21:43 +0800

    feat(#encrypt): add methods to make digital signature and uuid

--- src/main/java/com/akira/kioku/utils/TokenUtil.java
+++ src/main/java/com/akira/kioku/utils/TokenUtil.java
@@ -2,6 +2,8 @@ package com.akira.kioku.utils;
 
 import org.apache.shiro.crypto.hash.SimpleHash;
 
+import java.util.UUID;
+
 /**
  * 生成加密凭证
  * @author Kripath
@@ -27,4 +29,25 @@ public class TokenUtil {
     public static SimpleHash makeTEncryptTokenBySaltedMd5(String credential, String salt) {
         return new SimpleHash(HASH_ALGORITHM, credential, salt, HASH_TIMES);
     }
+
+    /**
+     * 生成UUID
+     * @return UUID
+     */
+    public static String makeUUID() {
+        return UUID.randomUUID().toString();
+    }
+
+    /**
+     * 生成数字签名
+     * @param username 用户名
+     * @param expireDate 过期时间（当前时间 + 链接有效时间）
+     * @param secretKey 密钥（随机字符串）
+     * @return 加密后的数字签名
+     */
+    public static String makeDigitalSignature(String username, Long expireDate, String secretKey) {
+        String sid = username + "$" + expireDate + "$" + secretKey;
+        return new SimpleHash(HASH_ALGORITHM, sid, null, HASH_TIMES).toString();
+    }
+
 }
