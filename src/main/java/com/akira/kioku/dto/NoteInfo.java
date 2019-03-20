0c5238c3dc68ce7080c87eb559f313872d71ae3a
mayoi7 <3451669860@qq.com>
2019-03-20 14:51:13 +0800

    fix(#info): fix bug of serializable

--- src/main/java/com/akira/kioku/dto/NoteInfo.java
+++ src/main/java/com/akira/kioku/dto/NoteInfo.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.util.Date;
 
 /**
@@ -15,7 +16,7 @@ import java.util.Date;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class NoteInfo {
+public class NoteInfo implements Serializable {
 
     private Long id;
 
--- src/main/java/com/akira/kioku/dto/UserDetail.java
+++ src/main/java/com/akira/kioku/dto/UserDetail.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.util.Date;
 
 /**
@@ -16,7 +17,7 @@ import java.util.Date;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class UserDetail {
+public class UserDetail implements Serializable {
 
     /** 主键 */
     private Long id;
